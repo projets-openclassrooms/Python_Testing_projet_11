0beaa816a304c6c150df84b010c40196c701f60c
bk7191 <bk7191014@gmail.com>
2024-08-21 16:15:45 +0200

    tests integrations

---
+++ tests/tests_integration/tests_purchaseplaces.py
@@ -0,0 +1,108 @@
+import pytest
+from flask_testing import TestCase
+import json
+import os
+import sys
+import pytest
+from server import app, clubs
+from settings import *
+
+
+
+
+
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    app.secret_key = 'something_special'
+    client = app.test_client()
+    return client
+
+
+@pytest.mark.integtest
+class FunctionalTest(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        app.secret_key = 'something_special'
+        return app
+
+    def setUp(self):
+        with open("competitions.json", "r") as f:
+            self.competitions = json.load(f)["competitions"]
+        with open("clubs.json", "r") as f:
+            self.clubs = json.load(f)["clubs"]
+        app.competitions = self.competitions
+        app.clubs = self.clubs
+
+    def test_purchase_places(self):
+        data = {
+            "competition": "Spring Festival",
+            "club": "Simply Lift",
+            "places": "1",
+        }
+        response = self.client.post("/purchasePlaces", data=data)
+        assert response.status_code == 200
+
+    def test_point_decrement(self):
+        club = next(c for c in self.clubs if c['name'] == "Simply Lift")
+        initial_point = int(club["points"])
+        data = {
+            "competition": "Spring Festival",
+            "club": "Simply Lift",
+            "places": "1",
+        }
+        response = self.client.post("/purchasePlaces", data=data)
+        assert response.status_code == 200
+
+        with open("clubs.json", "r") as f:
+            updated_clubs = json.load(f)["clubs"]
+        updated_club = next(c for c in updated_clubs if c['name'] == "Simply Lift")
+        updated_point = int(updated_club["points"])
+        assert updated_point == initial_point - 1
+
+    def test_purchasePlaces_not_enough_points(self):
+        data = {
+            "competition": "Spring Festival",
+            "club": "Iron Temple",
+            "places": "12",
+        }
+        response = self.client.post("/purchasePlaces", data=data)
+        assert response.status_code == 400
+
+    def test_purchasePlaces_negative_places(self):
+        data = {
+            "competition": "Spring Festival",
+            "club": "Simply Lift",
+            "places": "-3",
+        }
+        response = self.client.post("/purchasePlaces", data=data)
+        assert response.status_code == 400
+
+    def test_purchasePlaces_max_places_exceeded(self):
+        data = {
+            "competition": "Spring Festival",
+            "club": "Iron Temple",
+            "places": "15",
+        }
+        response = self.client.post("/purchasePlaces", data=data)
+        assert response.status_code == 400
+
+    def test_purchasePlaces_too_many_places(self):
+        data = {
+            "competition": "Spring Festival",
+            "club": "Simply Lift",
+            "places": "30",
+        }
+        response = self.client.post("/purchasePlaces", data=data)
+        assert response.status_code == 400
+
+    def test_purchasePlaces_no_places_specified(self):
+        data = {
+            "competition": "Spring Festival",
+            "club": "Simply Lift",
+            "places": "0",
+        }
+        response = self.client.post("/purchasePlaces", data=data)
+        assert response.status_code == 200
