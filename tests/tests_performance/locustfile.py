1a9e4fe81570271215229cc1c0b49fc042685126
bk7191 <bk7191014@gmail.com>
2024-08-21 15:43:34 +0200

    tests performances locust

--- tests/tests_performance/locustfile.py
+++ tests/tests_performance/locustfile.py
@@ -1,6 +1,7 @@
 from locust import HttpUser, task, between
 from random import choice
 
+
 class WebsiteUser(HttpUser):
     wait_time = between(1, 5)
 
@@ -10,7 +11,6 @@ class WebsiteUser(HttpUser):
             "admin@irontemple.com",
             "kate@shelifts.co.uk"
         ]
-
     @task(1)
     def index_page(self):
         self.client.get("/")
@@ -19,36 +19,32 @@ class WebsiteUser(HttpUser):
     def show_summary(self):
         email = choice(self.club_emails)
         self.client.post("/showSummary", data={"email": email})
-
-    @task(3)
-    def book_competition(self):
-        email = choice(self.club_emails)
         response = self.client.post("/showSummary", data={"email": email})
         if "Welcome" in response.text:
             club_name = response.text.split("Welcome, ")[1].split("!")[0]
             competitions = ["Spring Festival", "Fall Classic"]
             competition = choice(competitions)
             self.client.get(f"/book/{competition}/{club_name}")
+    #
+    # @task(4)
+    # def purchase_places(self):
+    #     email = choice(self.club_emails)
+    #     response = self.client.post("/showSummary", data={"email": email})
+    #     if "Welcome" in response.text:
+    #         club_name = response.text.split("Welcome, ")[1].split("!")[0]
+    #         competitions = ["Spring Festival", "Fall Classic"]
+    #         competition = choice(competitions)
+    #         places = choice(range(1, 13))
+    #         self.client.post("/purchasePlaces", data={
+    #             "competition": competition,
+    #             "club": club_name,
+    #             "places": places
+    #         })
+    #
+    # @task(5)
+    # def display_dashboard(self):
+    #     self.client.get("/dashboard")
 
-    @task(4)
-    def purchase_places(self):
-        email = choice(self.club_emails)
-        response = self.client.post("/showSummary", data={"email": email})
-        if "Welcome" in response.text:
-            club_name = response.text.split("Welcome, ")[1].split("!")[0]
-            competitions = ["Spring Festival", "Fall Classic"]
-            competition = choice(competitions)
-            places = choice(range(1, 13))
-            self.client.post("/purchasePlaces", data={
-                "competition": competition,
-                "club": club_name,
-                "places": places
-            })
-
-    @task(1)
-    def display_dashboard(self):
-        self.client.get("/dashboard")
-
-    @task(1)
-    def logout(self):
-        self.client.get("/logout")
+    # @task(6)
+    # def logout(self):
+    #     self.client.get("/logout")
