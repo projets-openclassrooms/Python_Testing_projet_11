f3a353a79e900b9ad80e10b49362e71775dd86e1
bk7191 <bk7191014@gmail.com>
2024-08-21 14:44:48 +0200

    tests performances locust

--- tests/tests_performance/locustfile.py
+++ tests/tests_performance/locustfile.py
@@ -1,30 +1,54 @@
-from locust import HttpUser, TaskSet, task, between
+from locust import HttpUser, task, between
+from random import choice
 
-class UserBehavior(TaskSet):
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 5)
+
+    def on_start(self):
+        self.club_emails = [
+            "john@simplylift.co",
+            "admin@irontemple.com",
+            "kate@shelifts.co.uk"
+        ]
+
     @task(1)
-    def index(self):
+    def index_page(self):
         self.client.get("/")
 
     @task(2)
     def show_summary(self):
-        self.client.post("/showSummary", data={"email": "test@example.com"})
+        email = choice(self.club_emails)
+        self.client.post("/showSummary", data={"email": email})
 
     @task(3)
-    def book(self):
-        self.client.get("/book/CompetitionName/ClubName")
+    def book_competition(self):
+        email = choice(self.club_emails)
+        response = self.client.post("/showSummary", data={"email": email})
+        if "Welcome" in response.text:
+            club_name = response.text.split("Welcome, ")[1].split("!")[0]
+            competitions = ["Spring Festival", "Fall Classic"]
+            competition = choice(competitions)
+            self.client.get(f"/book/{competition}/{club_name}")
 
     @task(4)
     def purchase_places(self):
-        self.client.post("/purchasePlaces", data={
-            "competition": "CompetitionName",
-            "club": "ClubName",
-            "places": "2"
-        })
+        email = choice(self.club_emails)
+        response = self.client.post("/showSummary", data={"email": email})
+        if "Welcome" in response.text:
+            club_name = response.text.split("Welcome, ")[1].split("!")[0]
+            competitions = ["Spring Festival", "Fall Classic"]
+            competition = choice(competitions)
+            places = choice(range(1, 13))
+            self.client.post("/purchasePlaces", data={
+                "competition": competition,
+                "club": club_name,
+                "places": places
+            })
 
-    @task(5)
-    def dashboard(self):
+    @task(1)
+    def display_dashboard(self):
         self.client.get("/dashboard")
 
-class WebsiteUser(HttpUser):
-    tasks = [UserBehavior]
-    wait_time = between(1, 5)  # Attendre entre 1 et 5 secondes entre les tâches
+    @task(1)
+    def logout(self):
+        self.client.get("/logout")
